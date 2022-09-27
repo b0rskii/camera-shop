61eccfda5e49e7948727b7b286889ebcc38b1fbe
Dmitry <gldimon@mail.ru>
2022-09-27 19:37:09 +0700

    Подготовил проект и страницы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import Layout from '../layout/layout';
+import CatalogPage from '../../pages/catalog-page/catalog-page';
+import ItemPage from '../../pages/item-page/item-page';
+import BasketPage from '../../pages/basket-page/basket-page';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <Layout>
+      <Routes>
+        <Route
+          path={AppRoute.Catalog}
+          element={<CatalogPage/>}
+        />
+        <Route
+          path={AppRoute.Item}
+          element={<ItemPage/>}
+        />
+        <Route
+          path={AppRoute.Basket}
+          element={<BasketPage/>}
+        />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/const.ts
@@ -0,0 +1,5 @@
+export const AppRoute = {
+  Catalog: '/',
+  Item: '/item',
+  Basket: '/basket',
+} as const;
